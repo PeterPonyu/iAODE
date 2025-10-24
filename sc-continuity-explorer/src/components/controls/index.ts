2aa49cb9ee991c94fcea69638f7098107651205a
ZeyuFu <fuzeyu09@gmail.com>
2025-10-24 17:28:38 +0800

    feat: continuity refined version

--- sc-continuity-explorer/src/components/controls/index.ts
+++
@@ -1,4 +0,0 @@
-export { ContinuitySlider } from './ContinuitySlider';
-export { TrajectorySelector } from './TrajectorySelector';
-export { EmbeddingSelector } from './EmbeddingSelector';
-export { ColorBySelector } from './ColorBySelector';
--- sc-continuity-explorer/src/components/layout/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Layout components barrel export
- */
-
-export { Sidebar } from './Sidebar';
-export type { SidebarProps } from './Sidebar';
-
-export { MainContent } from './MainContent';
-export type { MainContentProps } from './MainContent';
